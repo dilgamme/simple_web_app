0d85578da0040164f860ee9396ff92d9778d7daf
Dilgam Sharifov <dilgamsharifov@outlook.com>
2023-11-06 13:44:21 +0100

    updated

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# ... (other routes and configurations)
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
