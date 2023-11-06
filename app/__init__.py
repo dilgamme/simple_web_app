22b815da01be1a5a44011642e811c8fdca71d76e
Dilgam Sharifov <dilgamsharifov@outlook.com>
2023-11-06 13:57:54 +0100

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,5 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# from app import routes
-
-
-
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
+
 from app import routes
-
-# ... (other routes and configurations)
-
-@app.route('/courses')
-def courses():
-    return render_template('courses.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
